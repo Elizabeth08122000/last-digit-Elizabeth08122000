e69657bbdd10803b0e47b8f340d2ce90fa66994c
Liza <bliza0812@gmail.com>
2020-05-19 14:42:09 +0400

    Answer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner in = new Scanner(System.in);
+        int nom = in.nextInt();
+        System.out.println(nom%10);
+        in.close();
     }
 }
